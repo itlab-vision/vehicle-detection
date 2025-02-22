6f8c62f789eb4bc0aa5aa9ea7892a96d13da04e7
TepidmishA <perep.main@mail.ru>
2025-02-22 15:52:51 +0300

    Try to fix `Unable to import src.utils.data_reader`

--- src/accuracy_checker/accuracy_checker.py
+++ src/accuracy_checker/accuracy_checker.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
 from src.utils.data_reader import GroundtruthReader, DetectionReader
 
 
