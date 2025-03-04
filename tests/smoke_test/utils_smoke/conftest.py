4a0fa173efaf652bd6d7188a4bff949e44bff7a9
TepidmishA <perep.main@mail.ru>
2025-03-04 13:51:09 +0300

    Change utils_smoke/conftest.py docstring and Path using

--- tests/smoke_test/utils_smoke/conftest.py
+++ tests/smoke_test/utils_smoke/conftest.py
@@ -2,7 +2,9 @@
 Fixtures for Data Reader Modules.
 """
 
+import os
 import pytest
+from pathlib import Path
 
 
 @pytest.fixture
@@ -10,17 +12,19 @@ def valid_gt_csv(tmp_path):
     """
     Creates a temporary valid ground truth CSV file with correctly formatted entries.
 
-    Returns:
-        str: Path to the temporary ground truth CSV file.
-
     Content:
         1,CAR,10,10,20,20
         2,BUS,30,30,40,40
+
+    :return: Path to the temporary ground truth CSV file.
     """
-    csv_path = tmp_path / "valid_gt.csv"
+    str_path = os.path.join(tmp_path, "valid_gt.csv")
+    csv_path = Path(str_path)
+
     csv_content = "1,CAR,10,10,20,20\n2,BUS,30,30,40,40"
     csv_path.write_text(csv_content)
-    return str(csv_path)
+
+    return str_path
 
 
 @pytest.fixture
@@ -29,16 +33,18 @@ def invalid_gt_csv(tmp_path):
     Creates a temporary invalid ground truth CSV file with an incorrect format.
     This file contains an extra column that should not be present.
 
-    Returns:
-        str: Path to the temporary invalid ground truth CSV file.
-
     Content:
         1,CAR,10,10,20,20,extra
+
+    :return: Path to the temporary invalid ground truth CSV file.
     """
-    csv_path = tmp_path / "invalid_gt.csv"
+    str_path = os.path.join(tmp_path, "invalid_gt.csv")
+    csv_path = Path(str_path)
+
     csv_content = "1,CAR,10,10,20,20,extra\n"
     csv_path.write_text(csv_content)
-    return str(csv_path)
+
+    return str_path
 
 
 @pytest.fixture
@@ -46,17 +52,19 @@ def valid_detection_csv(tmp_path):
     """
     Creates a temporary valid detection CSV file with correctly formatted entries.
 
-    Returns:
-        str: Path to the temporary detection CSV file.
-
     Content:
         1,CAR,10,10,20,20,0.9
         2,BUS,30,30,40,40,0.8
+
+    :return: Path to the temporary detection CSV file.
     """
-    csv_path = tmp_path / "valid_det.csv"
+    str_path = os.path.join(tmp_path, "valid_det.csv")
+    csv_path = Path(str_path)
+
     csv_content = "1,CAR,10,10,20,20,0.9\n2,BUS,30,30,40,40,0.8"
     csv_path.write_text(csv_content)
-    return str(csv_path)
+
+    return str_path
 
 
 @pytest.fixture
@@ -64,13 +72,15 @@ def invalid_detection_csv(tmp_path):
     """
     Creates a temporary invalid detection CSV file missing the confidence score column.
 
-    Returns:
-        str: Path to the temporary invalid detection CSV file.
-
     Content:
-        1,CAR,10,10,20,20  # Missing Confidence column
+        1,CAR,10,10,20,20
+
+    :return: Path to the temporary invalid detection CSV file.
     """
-    csv_path = tmp_path / "invalid_det.csv"
-    csv_content = "1,CAR,10,10,20,20\n"  # Missing confidence
+    str_path = os.path.join(tmp_path, "invalid_det.csv")
+    csv_path = Path(str_path)
+
+    csv_content = "1,CAR,10,10,20,20\n"
     csv_path.write_text(csv_content)
-    return str(csv_path)
+
+    return str_path
