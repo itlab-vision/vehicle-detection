13b21f392549fea249d9cd24a98b69a03cedda0c
theProgrammerOfOurTime <azarova.v2104@gmail.com>
2025-03-14 12:26:44 +0300

    The model parameters are displayed. Added yaml file processing. The processing of the model output is placed in a separate class.

---
+++ samples/cli_main.py
@@ -0,0 +1,119 @@
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
+from src.gui_application.visualizer import Visualize
+from src.utils.data_reader import GroundtruthReader, FakeGroundtruthReader
+from src.utils.frame_data_reader import FrameDataReader
+from src.vehicle_detector.detector import Detector
+import datetime
+import argparse
+import os
+from PIL import Image
+
+import cv2 as cv
+import numpy as np
+import yaml
+
+def cli_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-y', '--yaml',
+                        type=str,
+                        help = 'Path to a yaml file',
+                        dest='yamlFile',
+                        required=False)
+    parser.add_argument('-t', '--mode',
+                        help='Mode (\'image\', \'video\')',
+                        type=str,
+                        dest='mode',
+                        choices=['image', 'video'],
+                        required=False)
+    parser.add_argument('-v', '--video',
+                        help='Path to a video file',
+                        type=str,
+                        dest='video_path')
+    parser.add_argument('-i', '--image',
+                        help='Path to images',
+                        type=str,
+                        dest='images_path',
+                        required=False)
+    parser.add_argument('-g', '--groundtruth',
+                        help='Path to a file of groundtruth',
+                        type=str,
+                        dest='groundtruth_path',
+                        required=False)
+    parser.add_argument('-m', '--model',
+                        help='Model (\'MobileNet\')',
+                        type=str,
+                        dest='model',
+                        choices=['MobileNet', 'YOLOv3-tiny'],
+                        required=False,
+                        default='MobileNet')
+    parser.add_argument('-cl', '--path_classes',
+                        help='Path to a file of classes',
+                        type=str,
+                        dest='path_classes',
+                        required=False)
+    parser.add_argument('-wp', '--path_weights',
+                        help='Path to a file of weights',
+                        type=str,
+                        dest='path_weights',
+                        required=False)
+    parser.add_argument('-cp', '--path_config',
+                        help='Path to a file of config',
+                        type=str,
+                        dest='path_config',
+                        required=False)
+    parser.add_argument('-c', '-conf', 
+                        help='Confidence threshold',
+                        type=float, 
+                        dest='conf',
+                        required=False)
+    parser.add_argument('-n', '-nms',
+                        help='Overlap threshold',
+                        type=float, 
+                        dest='nms',
+                        required=False)
+    parser.add_argument('-me', '-mean',
+                        help='The average picture',
+                        nargs=3,
+                        type = float,
+                        dest='mean',
+                        required=False)
+    
+    args = parser.parse_args()
+    return args
+
+#некоторое подобие main 
+def main():
+   
+    args = cli_argument_parser()
+    
+    if args.yamlFile != None:
+        
+        with open(args.yamlFile) as fh:
+            data = yaml.safe_load(fh)
+        data = data[0]
+        
+        reader = 0
+        if data['mode'] == 'image':
+            reader = FrameDataReader.create(data['mode'], data['images_path'])
+        elif data['mode'] == 'video':
+            reader = FrameDataReader.create(data['mode'], data['video_path'])
+            
+        adapter = None
+        detector = Detector.create( data['model'], data['path_classes'], data['path_weights'], data['path_config'], float(data['conf']), float(data['nms']), list(map(int, data['mean'].split(' '))))
+        visualizer = Visualize( reader, detector, GroundtruthReader().read(data['groundtruth_path']) )
+        visualizer.show()
+        
+    else:
+        reader = FrameDataReader.create( args.mode, (args.video_path or args.images_path) )
+        adapter = None
+        detector = Detector.create( args.model, args.path_classes, args.path_weights, args.path_config, args.conf, args.nms, args.mean)
+        visualizer = Visualize( reader, detector, GroundtruthReader().read(args.groundtruth_path) )
+        visualizer.show()
+
+if __name__ == '__main__':
+    main()
--- src/vehicle_detector/detector.py
+++ src/vehicle_detector/detector.py
@@ -1,107 +1,30 @@
 import cv2 as cv
+from cv2.dnn import DNN_BACKEND_CUDA, DNN_BACKEND_OPENCV
 import numpy as np
 import random
 from abc import ABC, abstractmethod
 
-class Detector(ABC):
+class Adapter(ABC):
     
     @abstractmethod
-    def detect(self, image):
+    def postProcessing(self, output, image_width, image_height):
         pass
+
+class AdapterMobileNet(Adapter):
     
-    @staticmethod
-    def create(model_path, model):
-        if model == 'MobileNet':
-            return VehicleDetector(model_path)
-        elif model == 'YOLOv3-tiny':
-            return VehicleDetectorYOLO(model_path)
-        elif model == "fake":
-            return FakeDetector()
-        else:
-            raise ValueError(f"Unsupported model: {model}")
+    def __init__(self, conf, nms, class_names):
+        self.conf = conf
+        self.nms = nms
+        self.class_names = class_names
 
-class VehicleDetectorYOLO(Detector):
-    
-    def __init__(self, model_path):
-        self.path_classes = model_path + "classes_coco.txt"
-        self.path_weights = model_path + "yolov3-tiny.weights"
-        self.path_config = model_path + "yolov3-tiny.cfg"
-        with open(self.path_classes, 'r') as f:
-            self.class_names = f.read().split('\n')
-
-        self.model = cv.dnn.readNet(model = self.path_weights, config = self.path_config, framework = 'Darknet')
-        
-    def detect(self, image):
-        
-        image_height, image_width, _ = image.shape
-        blob = cv.dnn.blobFromImage(image=image, scalefactor=(1.0 / 255.0), size=(416, 416), mean=(104, 117, 123), swapRB = True, crop=False)
-
-        self.model.setInput(blob)
-        output = self.model.forward()
-        
+    def postProcessing(self, output, image_width, image_height):
         classes_id = []
         boxes = []
         confidences = []
-        conf = 0.3
-        for detection in output:
-            
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            class_name = self.class_names[class_id]
-            if confidence > conf:
-                
-                сx1 = int(detection[0] * image_width)
-                cy1 = int(detection[1] * image_height)
-                
-                w = int(detection[2] * image_width)
-                h = int(detection[3] * image_height)          
-
-                if class_name == 'car' or class_name == 'bus' or class_name == 'truck':
-                    boxes.append((сx1 - w // 2, cy1 - h // 2, w, h))
-                    classes_id.append(class_name)
-                    confidences.append(confidence)
-                 
-        indexes = cv.dnn.NMSBoxes(boxes, confidences, conf, 0.4)
-        bboxes = []  
-        for i in indexes:
-            box = boxes[i]
-            x1 = box[0]
-            y1 = box[1]
-            w = box[2]
-            h = box[3]
-            bboxes.append((classes_id[i], x1, y1, x1 + w, y1 + h, confidences[i]))
-            
-        return bboxes
-
-
-class VehicleDetector(Detector):
-   
-    def __init__(self, model_path):
-        
-        self.path_classes = model_path + "classes_coco.txt"
-        self.path_inference = model_path + "frozen_inference_graph.pb"
-        self.path_config = model_path + "ssd_mobilenet_v2_coco_2018_03_29.pbtxt"
-        with open(self.path_classes, 'r') as f:
-            self.class_names = f.read().split('\n')
-
-        self.model = cv.dnn.readNet(model = self.path_inference, config = self.path_config, framework = 'TensorFlow')
-    
-    def detect(self, image):
-        
-        image_height, image_width, _ = image.shape
-        blob = cv.dnn.blobFromImage(image=image, size=(300, 300), mean=(104, 117, 123), swapRB=True)
-        self.model.setInput(blob)
-        output = self.model.forward()
-        
-        classes_id = []
-        boxes = []
-        confidences = []
-        conf = 0.3
         for detection in output[0, 0, :, :]:
             confidence = detection[2]
 
-            if confidence > conf:
+            if confidence > self.conf:
 
                 class_id = detection[1]
                 
@@ -118,7 +41,7 @@ class VehicleDetector(Detector):
                     classes_id.append(class_name)
                     confidences.append(confidence)
                    
-        indexes = cv.dnn.NMSBoxes(boxes, confidences, conf, 0.4)
+        indexes = cv.dnn.NMSBoxes(boxes, confidences, self.conf, self.nms)
         bboxes = []  
         for i in indexes:
             box = boxes[i]
@@ -129,6 +52,185 @@ class VehicleDetector(Detector):
             bboxes.append((classes_id[i], x1, y1, x1 + w, y1 + h, confidences[i]))
             
         return bboxes
+
+class AdapterYOLO(Adapter):
+    
+    def __init__(self, conf, nms, class_names):
+        self.conf = conf
+        self.nms = nms
+        self.class_names = class_names
+        
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def postProcessing(self, output, image_width, image_height):
+        classes_id = []
+        boxes = []
+        confidences = []
+        for detection in output:
+
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            class_name = self.class_names[class_id]
+            if confidence > self.conf:
+
+                сx1 = int(detection[0] * image_width)
+                cy1 = int(detection[1] * image_height)
+
+                w = int(detection[2] * image_width)
+                h = int(detection[3] * image_height)          
+
+                if class_name == 'car' or class_name == 'bus' or class_name == 'truck':
+                    boxes.append((сx1 - w // 2, cy1 - h // 2, w, h))
+                    classes_id.append(class_name)
+                    confidences.append(confidence)
+
+        indexes = cv.dnn.NMSBoxes(boxes, confidences, self.conf, self.nms)
+        bboxes = []  
+        for i in indexes:
+            box = boxes[i]
+            x1 = box[0]
+            y1 = box[1]
+            w = box[2]
+            h = box[3]
+            bboxes.append((classes_id[i], x1, y1, x1 + w, y1 + h, confidences[i]))
+
+        return bboxes
+    
+
+        # classes_id = []
+        # boxes = []
+        # confidences = []
+        # conf = 0.3
+        
+        # masks = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
+        # anchors = [[10, 13], [16, 30], [33, 23], [30, 61], [62, 45],
+        #        [59, 119], [116, 90], [156, 198], [373, 326]]
+        
+        # i = 0
+        # for out in output:
+            
+        #     mask = masks[i % 3]
+        #     i = i + 1
+        #     box_confidence = self.sigmoid(out[..., 4])
+        #     box_confidence = np.expand_dims(box_confidence, axis=-1)
+        #     box_class_probs = self.sigmoid(out[..., 5:])
+
+        #     box_scores = box_confidence * box_class_probs
+        #     class_id = np.argmax(box_scores, axis=-1)
+        #     confidence = np.max(box_scores, axis=-1)
+        #     class_name = self.class_names[class_id]
+        #     if confidence > conf:
+                
+               
+
+        #         if class_name == 'car' or class_name == 'bus' or class_name == 'truck':
+        #             # boxes.append((сx1 - w // 2, cy1 - h // 2, w, h))
+        #             # classes_id.append(class_name)
+        #             # confidences.append(confidence)
+                    
+        #             grid_h = 1
+        #             grid_w = 1 
+
+        #             anchors = [anchors[i] for i in mask]
+        #             anchors_tensor = np.array(anchors).reshape(1, 1, len(anchors), 2)
+
+        #             # Reshape to batch, height, width, num_anchors, box_params.
+        #             box_xy = self.sigmoid(out[..., :2])
+        #             box_wh = np.exp(out[..., 2:4])
+        #             box_wh = box_wh * anchors_tensor
+
+                    
+
+        #             col = np.tile(np.arange(0, grid_w), grid_w).reshape(-1, grid_w)
+        #             row = np.tile(np.arange(0, grid_h).reshape(-1, 1), grid_h)
+
+        #             col = col.reshape(grid_h, grid_w, 1, 1).repeat(3, axis=-2)
+        #             row = row.reshape(grid_h, grid_w, 1, 1).repeat(3, axis=-2)
+        #             grid = np.concatenate((col, row), axis=-1)
+
+        #             box_xy += grid
+        #             box_xy /= (grid_w, grid_h)
+        #             box_wh /= (416, 416)
+        #             box_xy -= (box_wh / 2.)
+        #             boxes = np.concatenate((box_xy, box_wh), axis=-1)
+                 
+        # indexes = cv.dnn.NMSBoxes(boxes, confidences, conf, 0.4)
+        # bboxes = []  
+        # for i in indexes:
+        #     box = boxes[i]
+        #     x1 = box[0]
+        #     y1 = box[1]
+        #     w = box[2]
+        #     h = box[3]
+        #     bboxes.append((classes_id[i], x1, y1, x1 + w, y1 + h, confidences[i]))
+            
+        # return bboxes
+    
+
+class Detector(ABC):
+    
+    @abstractmethod
+    def detect(self, image):
+        pass
+    
+    @staticmethod
+    def create(model, path_classes, path_weights, path_config, conf, nms, mean):
+        if model == 'MobileNet':
+            with open(path_classes, 'r') as f:
+                class_names = f.read().split('\n')
+            return VehicleDetectorMobileNet(path_weights, path_config, mean, AdapterMobileNet(conf, nms, class_names))
+        
+        elif model == 'YOLOv3-tiny':
+            with open(path_classes, 'r') as f:
+                class_names = f.read().split('\n')
+            return VehicleDetectorYOLO(path_weights, path_config, mean, AdapterYOLO(conf, nms, class_names))
+        
+        elif model == "fake":
+            return FakeDetector()
+        
+        else:
+            raise ValueError(f"Unsupported model: {model}")
+
+class VehicleDetectorYOLO(Detector):
+    
+    def __init__(self, path_weights, path_config, mean, adapter):
+        
+        self.mean = mean
+        self.adapter = adapter
+        self.model = cv.dnn.readNet(model = self.path_weights, config = self.path_config, framework = 'Darknet')
+        #self.model.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA) 
+        #self.model.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
+    
+    def detect(self, image):
+        
+        image_height, image_width, _ = image.shape
+        blob = cv.dnn.blobFromImage(image=image, scalefactor=(1.0 / 255.0), size=(416, 416), mean=self.mean, swapRB = True, crop=False)
+        self.model.setInput(blob)
+        output = self.model.forward()
+        
+        return self.adapter.postProcessing(output, image_width, image_height)
+
+class VehicleDetectorMobileNet(Detector):
+   
+    def __init__(self, path_weights, path_config, mean, adapter):
+        
+        self.mean = mean
+        self.adapter = adapter
+        self.model = cv.dnn.readNet(model = path_weights, config = path_config, framework = 'TensorFlow')
+        #self.model.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA) 
+        #self.model.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
+
+    def detect(self, image):
+        
+        image_height, image_width, _ = image.shape
+        blob = cv.dnn.blobFromImage(image = image, size = (300, 300), mean = self.mean, swapRB=True)
+        
+        self.model.setInput(blob)
+        output = self.model.forward()
+        
+        return self.adapter.postProcessing(output, image_width, image_height)
     
 class FakeDetector(Detector):
     def __init__(self, seed = None):
